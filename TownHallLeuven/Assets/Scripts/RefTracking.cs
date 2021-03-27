84de3a872b7f3906455c07285c7036fc25a83584
Vincent S <vincent.surkijn@student.kuleuven.be>
2021-03-27 13:53:48 +0100

    Ready to import scenes

--- TownHallLeuven/Assets/Scripts/RefTracking.cs
+++ TownHallLeuven/Assets/Scripts/RefTracking.cs
@@ -10,8 +10,11 @@ public class RefTracking : MonoBehaviour
 {
     private ARTrackedImageManager aRTrackedImageManager;
     public GameObject Info;
+    public GameObject spawn;
+    public ARSessionOrigin arOrig;
+    private Vector3 refPos;
+    private Quaternion refRot;
 
-    
     private void Awake()
     {
         aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
@@ -29,11 +32,16 @@ public class RefTracking : MonoBehaviour
 
     public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
     {
+        //ARSessionOrigin arOrig = GameObject.FindObjectOfType<ARSessionOrigin>();
         Info.GetComponent<Text>().text = "Camera is calibrated. Keep it like this.";
         Invoke(nameof(RemoveInfo), 5);
-        foreach(var trackedImage in args.added)
+        foreach(ARTrackedImage trackedImage in args.added)  // for each tracked image do this, but there will only be 1
         {
-            Debug.Log(trackedImage.name);
+            refPos = trackedImage.transform.position;
+            refRot = trackedImage.transform.rotation;
+            // spawn first object (person?)
+            spawn.SetActive(true);
+            spawn.transform.SetPositionAndRotation( refPos + Vector3.left * 0.04f, refRot);  // spawn object 0.04m to left
         }
     }
 
